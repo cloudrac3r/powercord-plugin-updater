16459f6016e36940bb12950502bdf9825a5a45d9
Cadence Fish <cloudrac3r@vivaldi.net>
2019-09-16 02:45:08 +1200

    Lint

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { React } = require('powercord/webpack');
 const { promisify } = require('util');
 const cp = require('child_process');
 const exec = promisify(cp.exec);
-const fsp = require("fs").promises;
+const fsp = require('fs').promises;
 
 const Settings = require('./components/Settings.jsx');
 
@@ -23,39 +23,39 @@ module.exports = class CadencePluginUpdater extends Plugin {
 
   async startPlugin () {
     this.loadCSS(resolve(__dirname, 'style.scss'));
-    this.registerSettings('powercord-plugin-updater', 'Plugin Updater', props => React.createElement(Settings, {...props, plugin: this}));
+    this.registerSettings('powercord-plugin-updater', 'Plugin Updater', props => React.createElement(Settings, { ...props,
+      plugin: this }));
   }
 
-  async getPluginInfo() {
-    let [pluginList, builtinListExec] = await Promise.all([
+  async getPluginInfo () {
+    const [ pluginList, builtinListExec ] = await Promise.all([
       fsp.readdir(this.cwd.cwd),
-      exec("git ls-files", this.cwd)
+      exec('git ls-files', this.cwd)
     ]);
-    let builtinList = builtinListExec.stdout.split("\n").map(line => line.split("/")[0]).filter((item, index, array) => (item && array.indexOf(item) == index));
-    let info = [];
+    const builtinList = builtinListExec.stdout.split('\n').map(line => line.split('/')[0]).filter((item, index, array) => (item && array.indexOf(item) === index));
+    const info = [];
     await Promise.all(pluginList.map(async p => {
-      let dirStat = await fsp.stat(join(this.cwd.cwd, p));
+      const dirStat = await fsp.stat(join(this.cwd.cwd, p));
       console.log(dirStat);
       if (dirStat.isDirectory()) {
-        let isBuiltIn = builtinList.includes(p);
-        let gitExists = await fsp.stat(join(this.cwd.cwd, p, ".git")).then(() => true).catch(() => false)
+        const isBuiltIn = builtinList.includes(p);
+        const gitExists = await fsp.stat(join(this.cwd.cwd, p, '.git')).then(() => true).catch(() => false);
         info.push({
           name: p,
           git: gitExists,
           builtin: isBuiltIn,
           icon:
             gitExists
-            ? "tick"
-            : isBuiltIn
-            ? "dot"
-            : "cross"
-          ,
+              ? 'tick'
+              : isBuiltIn
+                ? 'dot'
+                : 'cross',
           priority:
             gitExists
-            ? 0
-            : isBuiltIn
-            ? 2
-            : 1
+              ? 0
+              : isBuiltIn
+                ? 2
+                : 1
         });
       }
     }));
@@ -64,88 +64,89 @@ module.exports = class CadencePluginUpdater extends Plugin {
     return info;
   }
 
-  async update(settings) {
-    settings.setState({updating: true});
+  async update (settings) {
+    settings.setState({ updating: true });
 
-    let pluginInfo = await this.getPluginInfo();
+    const pluginInfo = await this.getPluginInfo();
     pluginInfo.forEach(p => {
-      if (p.git) p.icon = "updating";
+      if (p.git) {
+        p.icon = 'updating';
+      }
       delete p.message;
       delete p.link;
       return p;
     });
-    settings.setState({pluginInfo});
+    settings.setState({ pluginInfo });
 
     let topIndex = 0;
-    let reloadAvailable = settings.state.reloadAvailable;
+    let { reloadAvailable } = settings.state;
     await Promise.all(pluginInfo.map(async p => {
       if (p.git) {
-        let cwd = {
+        const cwd = {
           cwd: join(this.cwd.cwd, p.name)
-        }
-        const log = thing => console.log(`[${p.name}]`, thing);
-        //log("Running git pull --ff-only in "+cwd.cwd);
+        };
+        /*
+         * const log = thing => console.log(`[${p.name}]`, thing);
+         * log("Running git pull --ff-only in "+cwd.cwd);
+         */
 
-        let result = await exec("git pull --ff-only --verbose", cwd).catch(e => ({
-          stdout: "",
-          stderr: e.message.slice(e.message.indexOf("\n")+1)
+        const result = await exec('git pull --ff-only --verbose', cwd).catch(e => ({
+          stdout: '',
+          stderr: e.message.slice(e.message.indexOf('\n') + 1)
         }));
         let moveToTop = false;
 
-        if (result.stdout == "Already up to date.\n" || result.stdout == "Already up-to-date.\n") {
-          p.icon = "tick";
-
-        } else if (result.stdout.includes("\nFast-forward\n")) {
-          let outLines = result.stdout.split("\n");
-          let hashes = outLines.find(l => l.startsWith("Updating ")).split(" ")[1].split("..");
-          let errLines = result.stderr.split("\n");
-          let remote = errLines.find(l => l.startsWith("From ")).split(" ")[1];
-          if (remote.startsWith("https://github.com/")) {
-            p.link = remote+"/compare/"+hashes[0]+"..."+hashes[1]
-            p.message = "Open changes on GitHub";
+        if (result.stdout === 'Already up to date.\n' || result.stdout === 'Already up-to-date.\n') {
+          p.icon = 'tick';
+        } else if (result.stdout.includes('\nFast-forward\n')) {
+          const outLines = result.stdout.split('\n');
+          const hashes = outLines.find(l => l.startsWith('Updating ')).split(' ')[1].split('..');
+          const errLines = result.stderr.split('\n');
+          const remote = errLines.find(l => l.startsWith('From ')).split(' ')[1];
+          if (remote.startsWith('https://github.com/')) {
+            p.link = `${remote}/compare/${hashes[0]}...${hashes[1]}`;
+            p.message = 'Open changes on GitHub';
           }
-          p.icon = "sparkles";
+          p.icon = 'sparkles';
           reloadAvailable = true;
           moveToTop = true;
-
-        } else if (result.stderr.includes("fatal: Not possible to fast-forward, aborting.\n")) {
-          p.message = "Cannot fast-forward due to unmerged local changes. You'll need to merge manually.";
-          p.icon = "failed";
+        } else if (result.stderr.includes('fatal: Not possible to fast-forward, aborting.\n')) {
+          p.message = 'Cannot fast-forward due to unmerged local changes. You\'ll need to merge manually.';
+          p.icon = 'failed';
           moveToTop = true;
-
-        } else if (result.stderr.includes("Please commit your changes or stash them before you merge.\n")) {
-          p.message = "Updating would overwrite local changes. You should commit or discard your current changes.";
-          p.icon = "failed";
+        } else if (result.stderr.includes('Please commit your changes or stash them before you merge.\n')) {
+          p.message = 'Updating would overwrite local changes. You should commit or discard your current changes.';
+          p.icon = 'failed';
           moveToTop = true;
-
-        } else if (result.stderr.includes("There is no tracking information for the current branch.\n")) {
-          p.message = "This local branch isn't tracking a remote branch, so there's nothing to pull from.";
-          p.icon = "tick";
+        } else if (result.stderr.includes('There is no tracking information for the current branch.\n')) {
+          p.message = 'This local branch isn\'t tracking a remote branch, so there\'s nothing to pull from.';
+          p.icon = 'tick';
           moveToTop = true;
-
-        } else if (result.stderr.includes("You are not currently on a branch.\n")) {
-          p.message = "You are not currently on a branch, so there's nowhere to pull from. Save your work to a branch, and checkout that branch.";
-          p.icon = "failed";
+        } else if (result.stderr.includes('You are not currently on a branch.\n')) {
+          p.message = 'You are not currently on a branch, so there\'s nowhere to pull from. Save your work to a branch, and checkout that branch.';
+          p.icon = 'failed';
           moveToTop = true;
-
         } else {
-          p.message = "I'm not really sure what happened here. You should investigate the situation manually.";
-          p.icon = "unknown";
+          p.message = 'I\'m not really sure what happened here. You should investigate the situation manually.';
+          p.icon = 'unknown';
           moveToTop = true;
         }
 
         if (moveToTop) {
-          let index = pluginInfo.indexOf(p);
-          if (index == -1) throw new Error("Yo how is the index -1, that's not possible");
+          const index = pluginInfo.indexOf(p);
+          if (index === -1) {
+            throw new Error('Yo how is the index -1, that\'s not possible');
+          }
           pluginInfo.splice(index, 1);
           pluginInfo.splice(topIndex++, 0, p);
         }
 
-        //log(result);
-        settings.setState({pluginInfo});
+        // log(result);
+        settings.setState({ pluginInfo });
       }
     }));
 
-    settings.setState({updating: false, reloadAvailable});
+    settings.setState({ updating: false,
+      reloadAvailable });
   }
 };
